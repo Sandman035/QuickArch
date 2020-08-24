07076669e21eb63170120e2668d99045f8dbbb3c
siketcsalad <siketcsalad@pop-os.localdomain>
2020-08-24 19:27:00 -0400

    master

--- QuickArch.sh
+++ QuickArch.sh
@@ -152,38 +152,38 @@ genfstab -U /mnt >> /mnt/etc/fstab
 #change root into the new system
 arch-chroot /mnt /bin/bash -c 'echo "Please enter time zone: (example Canada)"; read ZONE; echo "Please enter time subzone: (example Eastern)"; read SUBZONE; timedatectl set-timezone ${Zone}/${SubZone}; ln -sf /usr/share/zoneinfo/${ZONE}/${SUBZONE} /etc/localtime; echo "-- pick locale --"; nano /etc/locale.gen; locale-gen; echo "Please enter hostname:"; read HOSTNAME; echo ${HOSTNAME} > /etc/hostname; pacman -S networkmanager dhclient --noconfirm --needed; systemctl enable NetworkManager.service; passwd; pacman -S grub-bios --noconfirm --needed'
 
-#ask for time zone
+##ask for time zone
 #echo "Please enter time zone: (example Canada)"
 #read ZONE
 
 #echo "Please enter time subzone: (example Eastern)"
 #read SUBZONE
 
-#set the time zone
+##set the time zone
 #timedatectl set-timezone ${Zone}/${SubZone}
 #ln -sf /usr/share/zoneinfo/${ZONE}/${SUBZONE} /etc/localtime
 
 #echo "-- pick locale --"
 
-#edit /etc/locale.gen
+##edit /etc/locale.gen
 #nano /etc/locale.gen
 
 #generate locales
 #locale-gen
 
-#generate hostname
+##generate hostname
 #echo "Please enter hostname:"
 #read HOSTNAME
 #echo ${HOSTNAME} > /etc/hostname
 
-#install network manager
+##install network manager
 #pacman -S networkmanager dhclient --noconfirm --needed
 #systemctl enable NetworkManager.service
 
-#set the root password
+##set the root password
 #passwd
 
-#install grub
+##install grub
 #pacman -S grub-bios --noconfirm --needed
 
 if [ ${YESNOEFI} == "yes" ]
@@ -198,9 +198,6 @@ mkinitcpio -P
 
 #create grub config file
 grub-mkconfig -o /boot/grub/grub.cfg
-
-#exit out of chroot
-exit
 
 echo "------------------------------------------------"
 echo "--            You may now reboot              --"
