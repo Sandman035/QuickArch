08019c9e35e3afd917a2495be3ec048592096a1b
siketcsalad <siketcsalad@pop-os.localdomain>
2020-08-25 21:26:14 -0400

    master

--- QuickArch.sh
+++ QuickArch.sh
@@ -117,7 +117,12 @@ echo "--         Arch Install on Main Drive         --"
 echo "------------------------------------------------"
 
 #install arch with some other packages
-pacstrap /mnt base base-devel linux linux-firmware nano sudo --noconfirm --needed
+pacstrap /mnt base base-devel linux linux-firmware nano sudo git --noconfirm --needed
+
+#install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 
 echo "------------------------------------------------"
 echo "--           Configure the system             --"
