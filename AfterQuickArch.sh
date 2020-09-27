28828bb732a7d81a070117656ea49ee4e5d66535
Sandman035 <70038749+Sandman035@users.noreply.github.com>
2020-09-27 10:27:03 -0400

    Update AfterQuickArch.sh

--- AfterQuickArch.sh
+++ AfterQuickArch.sh
@@ -38,4 +38,4 @@ echo "Please Enter the number which coresponds with the desktop environment you 
 echo "1 : Gnome     4 : Cinnamon       7 : LXDE     10 : Sugar   13 : None"
 echo "2 : KDE       5 : Deepin         8 : LXQt     11 : UKUI"
 echo "3 : Budgie    6 : Enlightenment  9 : MATE     12 : Xfce"
-read ${DE}
+read DE
