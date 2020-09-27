6c2cf330aca6f5067ef55a803198582e056def72
Sandman035 <70038749+Sandman035@users.noreply.github.com>
2020-09-27 14:26:03 -0400

    Update AfterQuickArch.sh

--- AfterQuickArch.sh
+++ AfterQuickArch.sh
@@ -56,3 +56,15 @@ echo "3 : Budgie    6 : Enlightenment  9 : MATE     12 : Xfce"
 
 read DE
 
+echo "--           Window Manager Install            --"
+echo "                                                 "
+
+#ask for the Window Manager which the user will want to install
+echo "Please Enter the number which coresponds with the window manager you wish to install:"
+echo "1 : Awesome         6 : Openbox    11 : Qtile       16 : Fluxbox   21 : JWM"
+echo "2 : i3              7 : Bswm       12 : xmonad      17 : FVWM      22 : lwm"
+echo "3 : i3-gaps         8 : spectrwm   13 : Blackbox    18 : IceWM     23 : Marco"
+echo "4 : Metacity        9 : WMW        14 : PekWm       19 : ukwm      24 : Xfwm"
+echo "5 : Herbstluftwm   10 : Notion     15 : Ratpoison   20 : sway      25 : none"
+
+read WM
